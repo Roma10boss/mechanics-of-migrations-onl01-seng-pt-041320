80f4a8ee61b4a696cad543e4527a0c9b4b7ae7ec
Romario Gustave <romagustave10@gmail.com>
2020-06-20 21:58:21 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_artists.rb
+++ db/migrate/01_create_artists.rb
@@ -4,13 +4,19 @@ ActiveRecord::Base.establish_connection(
 )
 
 
-
-class CreateArtists < ActiveRecord::Migration[5.2]
-  def change
-  create_table :artists do |t|
-    t.string :name
-     t.string :genre
-     t.integer :age
-     t.string :hometown
+class CreateArtists < ActiveRecord::Migration
+  def up
   end
-end
+
+  def down
+  end
+
+  def change
+    create_table :artists do |t|
+      t.string :name
+      t.string :genre
+      t.integer :age
+      t.string :hometown
+    end
+  end
+end 
